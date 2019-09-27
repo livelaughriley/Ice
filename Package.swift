ff136a814ea162e0e19feb8cfcd9a7e70b762c7a
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-09-27 16:50:19 -0500

    change swiftcli version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .package(url: "https://github.com/jakeheis/Icebox", from: "0.0.6"),
         .package(url: "https://github.com/kylef/PathKit", from: "0.9.0"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.1.1"),
-        .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.3.0"),
+        .package(url: "https://github.com/livelaughriley/SwiftCLI", from: "5.0.0"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable", from: "0.9.0"),
     ],
     targets: [
